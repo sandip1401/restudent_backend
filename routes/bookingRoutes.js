d58f69a49db60928de5acec518c19d0671c41e73
sandip1401 <159257894+sandip1401@users.noreply.github.com>
2025-11-24 01:32:19 +0530

    Update bookingRoutes.js

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -6,19 +6,17 @@ const router = express.Router();
 router.post("/book_table", async (req, res) => {
   try {
     const { name, email, date, time, guests } = req.body;
-    // Here you save to MongoDB (example only)
-    //await Booking.create({ name, email, date, time, guests });
 
-    //Email message
     const subject = "New Table Booking";
     const message = `New Booking Details:
-            Name:${name}
-            Email:${email}
-            Date:${date}
-            Time:${time}
-            Guests:${guests}`;
+Name: ${name}
+Email: ${email}
+Date: ${date}
+Time: ${time}
+Guests: ${guests}`;
+
     await sendEmail(subject, message, process.env.OWNER_EMAIL);
-    // 2. Email confirmation to User
+
     const userSubject = "Table Booking Confirmation";
     const userMessage = `Hello ${name},
 
@@ -33,9 +31,7 @@ Thank you for choosing our service!`;
 
     await sendEmail(userSubject, userMessage, email);
 
-    res
-      .status(200)
-      .json({ message: "Booking Created and Emails Sent Successfully" });
+    res.status(200).json({ message: "Booking Created and Emails Sent Successfully" });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: err.message });
